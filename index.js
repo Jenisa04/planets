06179d0fe2175cadb562a5faed86d6ccae87326b
Jenisa04 <57528657+Jenisa04@users.noreply.github.com>
2023-08-07 05:13:12 -0700

    Add files via upload
    All files uploaded including images

---
+++ index.js
@@ -0,0 +1,37 @@
+let msg = document.getElementById("text");
+let openD = document.getElementById("planet-list");
+let planet1 = document.getElementById("planet1");
+let planet2 = document.getElementById("planet2");
+let planet3 = document.getElementById("planet3");
+let planet4 = document.getElementById("planet4");
+let planet5 = document.getElementById("planet5");
+let planet6 = document.getElementById("planet6");
+let planet7 = document.getElementById("planet7");
+let planet8 = document.getElementById("planet8");
+
+
+function toPlanet1(){
+    openD.classList.add("open-card")
+}
+function toPlanet2(){
+    planet1.classList.add("open-card")
+}
+function toPlanet3(){
+    planet2.classList.add("open-card")
+}
+function toPlanet4(){
+    planet3.classList.add("open-card")
+}
+function toPlanet5(){
+    planet4.classList.add("open-card")
+}
+function toPlanet6(){
+    planet5.classList.add("open-card")
+}
+function toPlanet7(){
+    planet6.classList.add("open-card")
+}
+function toPlanet8(){
+    planet7.classList.add("open-card")
+}
+
